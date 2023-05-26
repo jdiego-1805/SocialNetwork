3d27bb49f5a878f08236856426b06a233336a401
jdiego-1805 <63172283+jdiego-1805@users.noreply.github.com>
2023-05-26 15:00:26 -0500

    fixed index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const ThoughtRoutes = require("./ThoughtRoutes");
-const UserRoutes = require("./UserRoutes");
+const ThoughtRoutes = require("./thoughtRoutes");
+const UserRoutes = require("./userRoutes");
 
 router.use("/Thoughts", ThoughtRoutes);
 router.use("/Users", UserRoutes);
