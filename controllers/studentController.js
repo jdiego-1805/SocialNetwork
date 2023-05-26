26cdb99c09ca7c1a52154a210489e5dded4e9658
jdiego-1805 <63172283+jdiego-1805@users.noreply.github.com>
2023-05-26 14:28:05 -0500

    starter

---
+++ controllers/studentController.js
@@ -0,0 +1,142 @@
+// ObjectId() method for converting UserId string into an ObjectId for querying database
+const { ObjectId } = require("mongoose").Types;
+const { User, Thought } = require("../models");
+
+// TODO: Create an aggregate function to get the number of Users overall
+const headCount = async () => {
+  // Your code here
+  const numberOfUsers = await User.aggregate();
+  return numberOfUsers;
+};
+
+// Execute the aggregate method on the User model and calculate the overall grade by using the $avg operator
+const grade = async (UserId) =>
+  User.aggregate([
+    // TODO: Ensure we include only the User who can match the given ObjectId using the $match operator
+    {
+      // Your code here
+    },
+    {
+      $unwind: "$assignments",
+    },
+    // TODO: Group information for the User with the given ObjectId alongside an overall grade calculated using the $avg operator
+    {
+      // Your code here
+    },
+  ]);
+
+module.exports = {
+  // Get all Users
+  async getUsers(req, res) {
+    try {
+      const Users = await User.find();
+      const UserObj = {
+        Users,
+        headCount: await headCount(),
+      };
+      return res.json(UserObj);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Get a single User
+  async getSingleUser(req, res) {
+    try {
+      const User = await User.findOne({ _id: req.params.UserId })
+        .select("-__v")
+        .lean();
+
+      if (!User) {
+        return res.status(404).json({ message: "No User with that ID" });
+      }
+
+      res.json({
+        User,
+        grade: await grade(req.params.UserId),
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // create a new User
+  async createUser(req, res) {
+    try {
+      const User = await User.create(req.body);
+      res.json(User);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Delete a User and remove them from the Thought
+  async deleteUser(req, res) {
+    try {
+      const User = await User.findOneAndRemove({ _id: req.params.UserId });
+
+      if (!User) {
+        return res.status(404).json({ message: "No such User exists" });
+      }
+
+      const Thought = await Thought.findOneAndUpdate(
+        { Users: req.params.UserId },
+        { $pull: { Users: req.params.UserId } },
+        { new: true }
+      );
+
+      if (!Thought) {
+        return res.status(404).json({
+          message: "User deleted, but no Thoughts found",
+        });
+      }
+
+      res.json({ message: "User successfully deleted" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Add an assignment to a User
+  async addAssignment(req, res) {
+    try {
+      console.log("You are adding an assignment");
+      console.log(req.body);
+      const User = await User.findOneAndUpdate(
+        { _id: req.params.UserId },
+        { $addToSet: { assignments: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      if (!User) {
+        return res
+          .status(404)
+          .json({ message: "No User found with that ID :(" });
+      }
+
+      res.json(User);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Remove assignment from a User
+  async removeAssignment(req, res) {
+    try {
+      const User = await User.findOneAndUpdate(
+        { _id: req.params.UserId },
+        { $pull: { assignment: { assignmentId: req.params.assignmentId } } },
+        { runValidators: true, new: true }
+      );
+
+      if (!User) {
+        return res
+          .status(404)
+          .json({ message: "No User found with that ID :(" });
+      }
+
+      res.json(User);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+const ThoughtSchema = new Schema(
+  {
+    ThoughtName: {
+      type: String,
+      required: true,
+    },
+    inPerson: {
+      type: Boolean,
+      default: true,
+    },
+    startDate: {
+      type: Date,
+      default: Date.now(),
+    },
+    endDate: {
+      type: Date,
+      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+    },
+    Users: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
